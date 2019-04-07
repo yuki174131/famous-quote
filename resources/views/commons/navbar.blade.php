0d5445c8a68b4fae8a6cc68eee012401b9ad8dfc
yuki.sekine <yuki31.messi10@gmail.com>
2019-04-07 09:44:55 +0000

    ログイン機能　ユーザー登録機能追加

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,8 +9,20 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+                @if (Auth::check())
+                    <li class="nav-item"><a href="#" class="nav-link">Users</a></li>
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li class="dropdown-item"><a href="#">My profile</a></li>
+                                <li class="dropdown-divider"></li>
+                                <li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>
+                            </ul>
+                        </li>
+                @else
+                <li class="nav-item">{!! link_to_route('signup.get', '新規登録', [], ['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class' => 'nav-link']) !!}</li>
+                @endif
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>ようこそ名言コレクションへ</h1>
+                <div class="mt-4">
+                    {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                </div>
+                <div class="mt-3">
+                    {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                </div>
+             </div>
+        </div>
         </div>
     </div>
+    @endif
 @endsection
