987931f91e43461acb3e554b58ee2a05e5699e26
yuki.sekine <yuki31.messi10@gmail.com>
2019-05-22 10:43:36 +0000

    バリデーションを英語から日本語へ

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
@@ -225,7 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        ' Debugbar'  =>  Barryvdh\Debugbar\Facade::class,
+        'Debugbar'  =>  Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
